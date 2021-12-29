e6724ab4665d211831f15794b485c77933e3c65b
hyeseonhan <h1225hs@gmail.com>
2021-12-29 23:08:39 +0900

    if문

---
+++ baekjoon/1330/app.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
+
+input = input[1].split(' ').map((item) => +item)
+
+solution(input[0], input[1])
+
+function solution(A, B){
+    if(A > B){
+        console.log(">")
+    }
+    if(A < B){
+        console.log("<")
+    }
+    if(A === B){
+        console.log("==")
+    }
+}
---
+++ baekjoon/2753/app.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
+
+solution(+input[0])
+
+function solution(year){
+    if( year % 4 === 0 && year % 100 !== 0 || year % 400 === 0){
+        console.log(1)
+        return
+    } console.log(0)
+}
+
+// function solution(year){
+//     if( year % 4 === 0 && year % 100 !== 0){
+//         console.log(1)
+//     } else if( year % 4 === 0 && year % 400 === 0){
+//         console.log(1)
+//     } else {console.log(0)}
+
+// }
