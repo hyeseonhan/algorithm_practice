014f11ba08b191fa44ff81d1707d658646f67849
hyeseonhan <h1225hs@gmail.com>
2022-01-02 00:31:42 +0900

    1차원 배열

--- baekjoon/4344/app.js
+++ baekjoon/4344/app.js
@@ -3,5 +3,43 @@ const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 let input = fs.readFileSync(filePath).toString().split('\n');
 
 const testCase = +input[0]
+const avgCase = [];
 
-f
+for(let i = 1; i <= testCase; i++){
+    const score = input[i].split(' ').map((item) => +item)
+  
+    const newscore = [];
+    for(let i = 1; i <= score[0]; i++){
+        newscore.push(score[i])
+    }
+    const testCases = {
+        N: score[0],
+        arr: newscore 
+    }
+    avgCase.push(testCases)
+}
+
+solution(testCase, avgCase)
+
+function solution(C, testCase){
+    for(let testCaseIdx = 0; testCaseIdx < C; testCaseIdx++){
+        const n = testCase[testCaseIdx].N
+        const arr = testCase[testCaseIdx].arr
+    
+        let scoreSum = 0;
+        for(let i = 0; i < n; i++){
+            scoreSum = scoreSum + arr[i]
+        }
+        const average = scoreSum / n
+
+        let cnt = 0;
+        for(let i = 0; i < n; i++){
+            if( average < arr[i]){
+                cnt++
+            }
+        }
+
+        const ratio = ((cnt / n) * 100).toFixed(3);
+        console.log(ratio + '%')
+    }
+}
