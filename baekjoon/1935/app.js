54760a18e2342efbc6a43d026f57703e35a66e80
hyeseonhan <h1225hs@gmail.com>
2022-01-13 23:48:31 +0900

    203 - 자료구조 1

---
+++ baekjoon/1935/app.js
@@ -0,0 +1,7 @@
+// 후위표기식
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+[n, ...nums] = input
+console.log(nums)
