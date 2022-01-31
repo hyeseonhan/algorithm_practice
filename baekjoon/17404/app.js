023f9ee7c46dd92e563131a9b59880207683a91e
hyeseonhan <h1225hs@gmail.com>
2022-01-31 23:36:44 +0900

    402 - 다이나믹 프로그래밍 1

---
+++ baekjoon/17404/app.js
@@ -0,0 +1,5 @@
+// RGB거리 2
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().trim().split('\n'),
+
